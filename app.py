161e47f1c12da3d1136d998a6b475e03ecf25673
meshackmokoinihi <meshackmokoinihi20@gmail.com>
2025-01-24 11:53:21 +0200

    updating gunicorn

--- app.py
+++ app.py
@@ -210,7 +210,8 @@ app.register_blueprint(wpdf.wPdf)
 
 
 if __name__ == '__main__':
-    app.run(host = '0.0.0.0' ,debug=True)            
+   
+    app.run(host = '0.0.0.0', port=10000)            
             
             
             
